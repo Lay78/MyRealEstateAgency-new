1d24eddf4d033cdbceeb0535b95aecfe1764bca0
Lay78 <antoine_lay@yahoo.fr>
2020-12-29 23:02:43 +0100

    create ViewModel of Simulator

--- mvvm/src/main/java/fr/efrei/reagency/view/SimulatorActivity.java
+++ mvvm/src/main/java/fr/efrei/reagency/view/SimulatorActivity.java
@@ -1,10 +1,14 @@
 package fr.efrei.reagency.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import fr.efrei.reagency.R;
+import fr.efrei.reagency.viewmodel.SimulatorActivityViewModel;
 
 import android.os.Bundle;
 
 public class SimulatorActivity extends AppCompatActivity {
+
+    private SimulatorActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ mvvm/src/main/java/fr/efrei/reagency/viewmodel/SimulatorActivityViewModel.java
@@ -0,0 +1,4 @@
+package fr.efrei.reagency.viewmodel;
+
+public class SimulatorActivityViewModel {
+}
