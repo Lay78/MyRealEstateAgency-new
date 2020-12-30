348b5f5c818e889357ef19063d7ee6312b97491a
Lay78 <antoine_lay@yahoo.fr>
2020-12-30 12:39:35 +0100

    implements formula of loan but not sure

--- mvvm/src/main/java/fr/efrei/reagency/view/SimulatorActivity.java
+++ mvvm/src/main/java/fr/efrei/reagency/view/SimulatorActivity.java
@@ -5,8 +5,25 @@ import fr.efrei.reagency.R;
 import fr.efrei.reagency.viewmodel.SimulatorActivityViewModel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class SimulatorActivity extends AppCompatActivity {
+
+    private EditText propertyPrice;
+    private EditText financialContribution;
+    private EditText loanRate;
+    private EditText loanDuration;
+
+    private Button button;
+
+    private TextView monthlyPayment;
+    private TextView totalPayments;
 
     private SimulatorActivityViewModel viewModel;
 
@@ -14,5 +31,49 @@ public class SimulatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulator);
+
+        propertyPrice = findViewById(R.id.propertyPrice);
+        financialContribution = findViewById(R.id.financialContribution);
+        loanRate = findViewById(R.id.loanRate);
+        loanDuration = findViewById(R.id.loanDuration);
+
+        button = findViewById(R.id.button);
+
+        monthlyPayment = findViewById(R.id.monthlyPayment);
+        totalPayments = findViewById(R.id.totalPayments);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateLoan();
+            }
+        });
+    }
+
+    public void calculateLoan(){
+        String et1, et2, et3, et4;
+
+        et1 = propertyPrice.getText().toString();
+        et2 = financialContribution.getText().toString();
+        et3 = loanRate.getText().toString();
+        et4 = loanDuration.getText().toString();
+
+        if (et1.matches("") || et2.matches("") || et3.matches("") || et4.matches("")) {
+            Toast.makeText(this, "You must complete all fields !", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            double loanAmount = Integer.parseInt(propertyPrice.getText().toString()) + Integer.parseInt(financialContribution.getText().toString());
+            double interestRate = Double.parseDouble(loanRate.getText().toString());
+            double loanPeriod = Integer.parseInt(loanDuration.getText().toString());
+            double r = interestRate / 1200;
+            double r1 = Math.pow(r + 1, loanPeriod);
+
+            double monthlyPaymentResult = (r + (r1 - 1)) * loanAmount;
+            double totalPaymentResult = monthlyPaymentResult * loanPeriod;
+
+            monthlyPayment.setText(new DecimalFormat("##.##").format(monthlyPaymentResult));
+            totalPayments.setText(new DecimalFormat("##.##").format(totalPaymentResult));
+        }
     }
 }
